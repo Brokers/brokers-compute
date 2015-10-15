31932654b0573b7711b08963b118bba3376c0f5b
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2015-10-15 14:58:44 -0500

    Add: Pages of conducts

--- pdf_report/controllers.js
+++ pdf_report/controllers.js
@@ -14,17 +14,19 @@ var options = {
     // Page options
     "border": "0",             // default is 0, units: mm, cm, in, px
 
-    // "footer": {
-    //   "height": "28mm",
-    //   "contents": '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>'
-    // },
-
     // File options
     "type": "pdf",             // allowed file types: png, jpeg, pdf
     "quality": "100",           // only used for types png & jpeg
 
     // Script options
     "timeout": 30000,           // Timeout that will cancel phantomjs, in milliseconds
+};
+
+var conductColors = {
+    'D': 'rgb(238, 48, 45)',
+    'I': 'rgb(125, 166, 72)',
+    'S': '#774491',
+    'C': '#0066B7',
 };
 
 /* Helper function to wrap Firebase's once 'value' calls into promises */
@@ -70,17 +72,37 @@ module.exports = {
             }            
         })
         .spread(function(test, userSs, mainConductSs, secondaryConductSs, naturalProfileSs, adaptedProfileSs) {
+            var results = test.results;
             var user = userSs.val();
             var mainConduct = mainConductSs.val();
             var secondaryConduct = secondaryConductSs.val();
             var naturalProfile = naturalProfileSs.val();
             var adaptedProfile = adaptedProfileSs.val();
 
+            var mainConductColor = conductColors[results.conducts[0]];
+            var secondaryConductColor = conductColors[results.conducts[1]];
+
             var context = {
                 "{{base_folder}}": path.join('file://', __dirname),
                 "{{fullname}}": user.name,
+
+                "{{main-conduct-letter}}": results.conducts[0],
                 "{{main-conduct-title}}": mainConduct.title,
+                "{{main-conduct-color}}": mainConductColor,
+                "{{main-conduct-best_leader}}": mainConduct['best_leader'],
+                "{{main-conduct-description}}": mainConduct['description'],
+                "{{main-conduct-desires}}": mainConduct['desires'],
+                "{{main-conduct-motivation}}": mainConduct['motivation'],
+                "{{main-conduct-to_learn}}": mainConduct['to_learn'],
+
+                "{{secondary-conduct-letter}}": results.conducts[1],
                 "{{secondary-conduct-title}}": secondaryConduct.title,
+                "{{secondary-conduct-color}}": secondaryConductColor,
+                "{{secondary-conduct-best_leader}}": secondaryConduct['best_leader'],
+                "{{secondary-conduct-description}}": secondaryConduct['description'],
+                "{{secondary-conduct-desires}}": secondaryConduct['desires'],
+                "{{secondary-conduct-motivation}}": secondaryConduct['motivation'],
+                "{{secondary-conduct-to_learn}}": secondaryConduct['to_learn'],
             };
 
             _(context).each(function(value, key) {
